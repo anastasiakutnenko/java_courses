c846efe34d2df9a28c1b1a05a3221b16470053a4
anastasiakutnenko <anastasia.kutnenko@gmail.com>
2018-03-19 17:05:36 +0200

    updated Main

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         PhoneValidator.getPhoneNumber();
-//        RecursiveFactorial factorial = new RecursiveFactorial();
-//        factorial.recursiveFactorial();
-////        Factorial.factorial();
+//        Factorial.factorial();
     }
 }
